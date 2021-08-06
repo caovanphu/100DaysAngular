fe3e911f06da142adf6155d7ccbc50f80a6be7f4
caovanphu <caovanphu@gmail.com>
2021-08-07 00:01:25 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-bnjyln
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-bnjyln)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  inputType = 'text';
+  user = {
+    name: 'Phu CV',
+    age: 30
+  };
+  handler(eveny) {
+    console.log('clicked', event);
+  }
+}
